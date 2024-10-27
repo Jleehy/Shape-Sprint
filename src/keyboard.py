61c09ca0429cf020735f1800eafa12a9269ae5f2
seanhammell <sean.m.hammell@gmail.com>
2024-10-27 15:47:15 -0500

    Add prologue comments for keyboard.py

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,20 +1,30 @@
 """
 keyboard.py
-Description: Keyboard class
+Description:
+    Provides getting and setting the pressed state of keyboard keys.
 Programmers:
     Steve Gan
     Sean Hammell
     Jacob Leehy
     Mario Simental
     Matthew Sullivan
-Created: Oct 23, 2024
+Created:
+    Oct 23, 2024
 Revisions:
+    Oct 27, 2024: Finalized prologue comments - Sean Hammell
 Preconditions:
+    The key parameter of is_key_down and set_key_down is a Pygame key constant.
+    is_down is a boolean.
 Postconditions:
+    The pressed state of a given key is recorded in the _keys dict.
 Error Conditions:
+    None.
 Side Effects:
+    None.
 Invariants:
+    There will never be more than one dict entry for a given key.
 Known Faults:
+    None.
 """
 
 
