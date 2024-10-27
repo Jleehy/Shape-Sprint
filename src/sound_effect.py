bdc4ac48beb276b089330286aba8a5fea442ff36
seanhammell <sean.m.hammell@gmail.com>
2024-10-27 15:49:42 -0500

    Add prologue comments to sound_effect.py

--- src/sound_effect.py
+++ src/sound_effect.py
@@ -1,20 +1,30 @@
 """
 sound_effect.py
-Description: SoundEffect class
+Description:
+    Provides a wrapper around a pygame.sound object.
 Programmers:
     Steve Gan
     Sean Hammell
     Jacob Leehy
     Mario Simental
     Matthew Sullivan
-Created: Oct 23, 2024
+Created:
+    Oct 23, 2024
 Revisions:
+    Oct 27, 2024: Finalized prologue comments - Sean Hammell
 Preconditions:
+    The Pygame library is initialized.
+    The file passed to the constructor is a valid .ogg or .wav file.
 Postconditions:
+    A sound effect is available to play.
 Error Conditions:
+    __init__ will raise an exception if the image file is invalid or missing.
 Side Effects:
+    None.
 Invariants:
+    None.
 Known Faults:
+    None.
 """
 
 import pygame
