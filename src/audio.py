a412633543aaffd1fd580214a936a845540c2df8
seanhammell <sean.m.hammell@gmail.com>
2024-10-27 15:32:54 -0500

    Fill out prologue comments for audio.py

--- src/audio.py
+++ src/audio.py
@@ -1,23 +1,36 @@
 """
 audio.py
-Description: Audio functions
+Description:
+    Provides functionality for loading, playing, and pausing music.
 Programmers:
     Steve Gan
     Sean Hammell
     Jacob Leehy
     Mario Simental
     Matthew Sullivan
-Created: Oct 23, 2024
+Created:
+    Oct 23, 2024
 Revisions:
+    Oct 27, 2024: Finalized prologue comments - Sean Hammell
 Preconditions:
+    pygame.init() has been called to initialize the Pygame library.
+    The file passed to set_music is a valid .ogg or .wav file.
 Postconditions:
+    The music is available to play, pause, and unpause.
 Error Conditions:
+    set_music will raise an exception if the music file is invalid or missing.
+    play_music, pause_music, and unpause_music will raise an exception if there is not
+        a valid music file loaded.
 Side Effects:
+    None
 Invariants:
+    There can never be more than a single music file loaded at a time.
 Known Faults:
+    None
 """
 
 import pygame
+
 
 def set_music(file):
     """
